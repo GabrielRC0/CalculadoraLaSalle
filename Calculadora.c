54327c61b3f41c7ba817058342f403536a15acda
GabrielRC0 <gabriel.camargoadv@gmail.com>
2022-10-24 14:59:18 -0300

    Trabalho finalizado

--- Calculadora.c
+++ Calculadora.c
@@ -1,8 +1,8 @@
-//GabrielRC0 La Salle 
+x//GabrielRC0 La Salle 
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-int main()  
+int main()  // Foi utilizado de referencia a calculadora desse site https://www.javatpoint.com/calculator-program-in-c
 {  
     // Declarar as variadas operações;  
     int op, n1, n2; 
@@ -14,94 +14,97 @@ int main()
     do  
     {  
         // Demonstra para o usuario a parte de escolha  
-        printf (" Select an operation to perform the calculation in C Calculator: ");  
-        printf (" \n 1 Addition  \t \t 2 Subtraction \n 3 Multiplication \t 4 Division \n 5 Square \t \t 6 Square Root \n 7 Exit \n \n Please, Make a choice ");      
+        printf (" Ola meu rei, essa calculadora aqui foi feita com amor e carinho por: \n Gabriel Ribeiro Camargo (202213168) do curso de Analise e Desenvolvimento de Sistemas da La Salle Canoas!");
+        printf (" Selecione a operação desejada ");  
+        printf (" \n 1 Soma  \t \t 2 Menos \n 3 Multiplicação \t 4 divisão \n 5 Potencia \t \t 6 raiz quadrada \n 7 Fechar \n \n Escolhe ai, Irmão ");      
           
-        scanf ("%d", &op); // accepts a numeric input to choose the operation  
+        scanf ("%d", &op); // aceita um valor para encaminhar a operação desejada  
       
       
-    // use switch statement to call an operation  
     switch (op)  
     {  
         case 1:  
-            // Add two numbers  
-            printf (" You chose: Addition");  
-            printf ("\n Enter First Number: ");  
+            // soma dois numeros  
+            printf (" Você escolheu: Soma");  
+            printf ("\n Digite o primeiro Numero: ");  
             scanf (" %d", &n1);  
-            printf (" Enter Second Number: ");  
+            printf (" Digite o segundo numero: ");  
             scanf (" %d", &n2);  
-            res = n1 + n2; // Add two numbers  
-            printf (" Addition of two numbers is: %.2f", res);  
+            res = n1 + n2; // soma dois numeros  
+            printf (" O resultado é: %.2f", res);  
             break; // break the function  
               
         case 2:  
-            // Subtract two numbers  
-            printf (" You chose: Subtraction");  
-            printf ("\n Enter First Number: ");  
+            // diminui 
+            printf (" Você escolheu: Menos");  
+            printf ("\n Digite o primeiro numero: ");  
             scanf (" %d", &n1);  
-            printf (" Enter Second Number: ");  
+            printf (" Digite o segundo numero: ");  
             scanf (" %d", &n2);  
-            res = n1 - n2; // subtract two numbers  
-            printf (" Subtraction of two numbers is: %.2f", res);  
-            break; // break the function  
+            res = n1 - n2; // Diminui dois numeros  
+            printf (" o resultado é: %.2f", res);  
+            break;  
               
         case 3:  
-            // Multiplication of the numbers  
-            printf (" You chose: Multiplication");  
-            printf ("\n Enter First Number: ");  
+            // Multiplica  
+            printf (" Voce escolheu: Multiplicação");  
+            printf ("\n Digite o primeiro numero: ");  
             scanf (" %d", &n1);  
-            printf (" Enter Second Number: ");  
+            printf (" Digite o segundo numero: ");  
             scanf (" %d", &n2);  
-            res = n1 * n2; // multiply two numbers  
-            printf (" Multiplication of two numbers is: %.2f", res);  
-            break; // break the function  
+            res = n1 * n2; // multiplica ambos  
+            printf (" O resultado é: %.2f", res);  
+            break; 
               
         case 4:  
-            // Division of the numbers  
-            printf (" You chose: Division");  
-            printf ("\n Enter First Number: ");  
+            // Divisão
+            printf (" Voce escolheu: divisão");  
+            printf ("\n Digite o primeiro numero ");  
             scanf (" %d", &n1);  
-            printf (" Enter Second Number: ");  
+            printf (" Digite o segundo numero: ");  
             scanf (" %d", &n2);  
             if (n2 == 0)  
                 {  
-                    printf (" \n Divisor cannot be zero. Please enter another value ");  
+                    printf (" \n BIP BOP ERROR ERROR, BOTE OUTRO DIVISOR SENÃO DEVEREI LIGAR AAUTODESTRUIÇÃO DO COMPUTADOR EM 3.2.1..... ");  
                     scanf ("%d", &n2);        
                 }  
-            res = n1 / n2; // divide two numbers  
-            printf (" Division of two numbers is: %.2f", res);  
-            break; // break the function  
+            res = n1 / n2; // divide dois n°  
+            printf (" O resultado é: %.2f", res);  
+            break;  
               
         case 5:  
-           printf("Enter a base number: ");
-    scanf("%d", &base);
-    printf("Enter an exponent: ");
-    scanf("%d", &exp);
+            // Potencia
+            printf (" Voce escolheu: Potencia"); 
+            printf(" Digite o primeiro numero: ");
+            scanf("%d", &base);
+            printf("Mete o expoente: ");
+            scanf("%d", &exp);
 
-    while (exp != 0) {
-        result *= base;
-        --exp;
-    }
-    printf("Answer = %.0Lf", result);
-            break; // break the function  
+            while (exp != 0) {
+            result *= base;
+            --exp;
+            }
+            printf("Answer = %.0Lf", result); // faz a potenciação
+            break;  
               
         case 6:  
-            // getting the square root of the number  
-            printf (" You chose: Square Root");  
-            printf ("\n Enter First Number: ");  
+            // Raiz quadrada grande!
+            printf (" Voce escolheu: Raiz quadrada");  
+            printf ("\n Digite o primeiro numero: ");  ++
+            ++++++++++++++++++++
             scanf (" %d", &n1);  
               
-            res = sqrt(n1); // use sqrt() function to find the Square Root   
-            printf (" Square Root of %d numbers is: %.2f", n1, res);  
-            break; // break the function  
+            res = sqrt(n1); // sqrt para ter a raiz quadrada  
+            printf (" A raiz quadrada de  %d é: %.2f", n1, res);  
+            break; 
               
         case 7:  
-            printf (" You chose: Exit");  
+            printf (" ATÉ LOGO AMIGO TENHA UM BOM DIA VIU");  
             exit(0);   
-            break; // break the function  
+            break; 
               
         default:  
-            printf(" Something is wrong!! ");  
+            printf(" AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA DEU ERRO PO!! ");  
             break;                        
     }  
     printf (" \n \n ********************************************** \n ");  
